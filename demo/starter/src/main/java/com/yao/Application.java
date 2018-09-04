e45c477c26b01ea7168ebb7f2fe843f4f1c6a40f
No.007 <yqjdcyy@gmail.com>
2018-09-04 23:14:45 +0800

    starter

--- demo/starter/src/main/java/com/yao/Application.java
+++ demo/starter/src/main/java/com/yao/Application.java
@@ -5,17 +5,22 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Creator: Yao
+ * Date:    2018/9/4
+ * For:
+ * Other:
+ */
 @RestController
 @EnableAutoConfiguration
 public class Application {
 
-    @RequestMapping("/")
-    String home(){
-        return "hello world";
+    @RequestMapping(value = {"", "/"})
+    String home() {
+        return "hello spring.boot";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
