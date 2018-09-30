51dc9f734ac423d598793576d0f8a2f1e1f3e4ad
No.007 <yqjdcyy@gmail.com>
2018-09-30 11:21:02 +0800

    active mq

---
+++ demo/utils/activemq/src/main/java/com/yao/Application.java
@@ -0,0 +1,19 @@
+package com.yao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Creator: Yao
+ * Date:    2018/9/30
+ * For:
+ * Other:
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
